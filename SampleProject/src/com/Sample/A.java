7a9b77077f90aa9f2af2028c3b47ca9031b890d7
SreevaniNandyala <SreevaniNandyala@IBM-7DXKB54>
2025-10-31 18:11:43 +0530

    new changes

--- SampleProject/src/com/Sample/A.java
+++ SampleProject/src/com/Sample/A.java
@@ -8,7 +8,7 @@ public class A {
 	
 	public static void main(String args[]) {
 	
-	List<String> list=Arrays.asList("the","focx","is","the","claver");
+	List<String> list=Arrays.asList("the","focx","is","the","claver","vani");
 	
 	
 	long num=IntStream.range(0, list.size()).filter(i->"the".equals(list.get(i))).count();
